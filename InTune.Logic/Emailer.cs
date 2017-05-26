1bde9ea63b93161233b744ed7f1bd7da244e3daa
ashokgudur <ashok.gudur@gmail.com>
2017-05-26 17:20:48 +0530

    Changed ForgotPassword API method

--- InTune.Logic/Emailer.cs
+++ InTune.Logic/Emailer.cs
@@ -12,30 +12,29 @@ namespace InTune.Logic
     {
         public static void SendMail(EmailMessage eMailMessage)
         {
-            return;
-            //var fromAddress = new MailAddress(eMailMessage.FromAddress, eMailMessage.FromAddressDisplayName);
-            //var smtp = new SmtpClient
-            //{
-            //    Host = "smtp.gmail.com",
-            //    Port = 587,
-            //    EnableSsl = true,
-            //    DeliveryMethod = SmtpDeliveryMethod.Network,
-            //    Credentials = new NetworkCredential(fromAddress.Address, eMailMessage.FromPassword),
-            //    Timeout = 200000
-            //};
+            var fromAddress = new MailAddress(eMailMessage.FromAddress, eMailMessage.FromAddressDisplayName);
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, eMailMessage.FromPassword),
+                Timeout = 200000
+            };
 
-            ////var rtfMail = AlternateView.CreateAlternateViewFromString(rtbBody.Rtf, null, "text/html");
-            //var toAddress = new MailAddress(eMailMessage.ToAddress, eMailMessage.ToAddressDisplayName);
-            //using (var message =
-            //    new MailMessage(fromAddress, toAddress)
-            //    {
-            //        Subject = eMailMessage.Subject,
-            //        Body = eMailMessage.Body
-            //    })
-            //{
-            //    //message.AlternateViews.Add(rtfMail);
-            //    smtp.Send(message);
-            //}
+            //var rtfMail = AlternateView.CreateAlternateViewFromString(rtbBody.Rtf, null, "text/html");
+            var toAddress = new MailAddress(eMailMessage.ToAddress, eMailMessage.ToAddressDisplayName);
+            using (var message =
+                new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = eMailMessage.Subject,
+                    Body = eMailMessage.Body
+                })
+            {
+                //message.AlternateViews.Add(rtfMail);
+                smtp.Send(message);
+            }
         }
     }
 }
